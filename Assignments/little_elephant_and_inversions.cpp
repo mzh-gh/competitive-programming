532f43edf11b635477dfe3bca62c7a73aed0ffe3
Mark Zhou <xinyunzhouu@gmail.com>
2022-07-05 01:07:11 -0400

    Commit test: unfinished problem

--- Assignments/little_elephant_and_inversions.cpp
+++ Assignments/little_elephant_and_inversions.cpp
@@ -7,4 +7,10 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
 }
