3865335df694c025351ce281a59cba23d78cef81
Mark Zhou <xinyunzhouu@gmail.com>
2022-07-05 01:06:38 -0400

    Commit test

---
+++ Assignments/little_elephant_and_inversions.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
