e613bae076c6c3dce956f811266e8fc59c414d17
Mark Zhou <xinyunzhouu@gmail.com>
2022-07-10 01:46:51 -0400

    Reformat range queryand range update segtrees

--- Library/Segtree/segtree_range_query.cpp
+++ Library/Segtree/segtree_range_query.cpp
@@ -4,65 +4,70 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)x.size()
 
-int sz;
-vector<long long> t;
+struct segtree {
+  int sz;
+  vector<long long> tree;
 
-void init(int n) {
-  sz = 1;
-  while (sz < n) {
-    sz *= 2;
-  }
-  t.resize(2 * sz);
-}
-
-void upd(int i, int v, int x, int l, int r) {
-  if (l + 1 == r) {
-    t[x] = v;
-    return;
-  }
-  int mid = l + (r - l) / 2;
-  if (i < mid) {
-    upd(i, v, 2 * x + 1, l, mid);
-  } else {
-    upd(i, v, 2 * x + 2, mid, r);
-  }
-  t[x] = t[2 * x + 1] + t[2 * x + 2];
-}
-
-void upd(int i, int v) {
-  upd(i, v, 0, 0, sz);
-}
-
-void build(vector<int> &a, int x, int l, int r) {
-  if (l + 1 == r) {
-    if (l < sz(a)) {
-      t[x] = a[l];
+  void init(int n) {
+    sz = 1;
+    while (sz < n) {
+      sz *= 2;
     }
-    return;
+    tree.resize(2 * sz);
   }
-  int mid = l + (r - l) / 2;
-  build(a, 2 * x + 1, l, mid);
-  build(a, 2 * x + 2, mid, r);
-  t[x] = t[2 * x + 1] + t[2 * x + 2];
-}
 
-void build(vector<int> &a) {
-  build(a, 0, 0, sz);
-}
-
-long long query(int L, int R, int x, int l, int r) {
-  if (l >= R || r <= L) {
-    return 0;
+  void upd(int i, int v, int x, int l, int r) {
+    if (l + 1 == r) {
+      tree[x] = v;
+      return;
+    }
+    int mid = l + (r - l) / 2;
+    if (i < mid) {
+      upd(i, v, 2 * x + 1, l, mid);
+    } else {
+      upd(i, v, 2 * x + 2, mid, r);
+    }
+    tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
   }
-  if (l >= L && r <= R) {
-    return t[x];
+
+  void upd(int i, int v) {
+    upd(i, v, 0, 0, sz);
   }
-  int mid = l + (r - l) / 2;
-  return query(L, R, 2 * x + 1, l, mid) + query(L, R, 2 * x + 2, mid, r);
-}
 
-long long query(int l, int r) {
-  return query(l, r, 0, 0, sz);
-}
+  void build(vector<int> &a, int x, int l, int r) {
+    if (l + 1 == r) {
+      if (l < sz(a)) {
+        tree[x] = a[l];
+      }
+      return;
+    }
+    int mid = l + (r - l) / 2;
+    build(a, 2 * x + 1, l, mid);
+    build(a, 2 * x + 2, mid, r);
+    tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
+  }
 
-int main() {}
+  void build(vector<int> &a) {
+    build(a, 0, 0, sz);
+  }
+
+  long long query(int L, int R, int x, int l, int r) {
+    if (l >= R || r <= L) {
+      return 0;
+    }
+    if (l >= L && r <= R) {
+      return tree[x];
+    }
+    int mid = l + (r - l) / 2;
+    return query(L, R, 2 * x + 1, l, mid) + query(L, R, 2 * x + 2, mid, r);
+  }
+
+  long long query(int l, int r) {
+    return query(l, r, 0, 0, sz);
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
--- Library/Segtree/segtree_range_update.cpp
+++ Library/Segtree/segtree_range_update.cpp
@@ -4,67 +4,69 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)x.size()
 
-int sz;
-vector<long long> t;
+struct segtree {
+  int sz;
+  vector<long long> tree;
 
-void init(int n) {
-  sz = 1;
-  while (sz < n) {
-    sz *= 2;
-  }
-  t.resize(2 * sz);
-}
-
-void upd(int L, int R, int v, int x, int l, int r) {
-  if (l >= R || r <= L) {
-    return;
-  }
-  if (l >= L && r <= R) {
-    t[x] += v;
-    return;
-  }
-  int mid = l + (r - l) / 2;
-  upd(L, R, v, 2 * x + 1, l, mid);
-  upd(L, R, v, 2 * x + 2, mid, r);
-}
-
-void upd(int l, int r, int v) {
-  upd(l, r, v, 0, 0, sz);
-}
-
-void build(vector<int> &a, int x, int l, int r) {
-  if (l + 1 == r) {
-    if (l < sz(a)) {
-      t[x] = a[l];
+  void init(int n) {
+    sz = 1;
+    while (sz < n) {
+      sz *= 2;
     }
-    return;
+    tree.resize(2 * sz);
   }
-  int mid = l + (r - l) / 2;
-  build(a, 2 * x + 1, l, mid);
-  build(a, 2 * x + 2, mid, r);
-}
 
-void build(vector<int> &a) {
-  build(a, 0, 0, sz);
-}
-
-long long query(int i, int x, int l, int r) {
-  if (l + 1 == r) {
-    return t[x];
+  void upd(int L, int R, int v, int x, int l, int r) {
+    if (l >= R || r <= L) {
+      return;
+    }
+    if (l >= L && r <= R) {
+      tree[x] += v;
+      return;
+    }
+    int mid = l + (r - l) / 2;
+    upd(L, R, v, 2 * x + 1, l, mid);
+    upd(L, R, v, 2 * x + 2, mid, r);
   }
-  long long ans = t[x];
-  int mid = l + (r - l) / 2;
-  if (i < mid) {
-    ans += query(i, 2 * x + 1, l, mid);
-  } else {
-    ans += query(i, 2 * x + 2, mid, r);
-  }
-  return ans;
-}
 
-long long query(int i) {
-  return query(i, 0, 0, sz);
-}
+  void upd(int l, int r, int v) {
+    upd(l, r, v, 0, 0, sz);
+  }
+
+  void build(vector<int> &a, int x, int l, int r) {
+    if (l + 1 == r) {
+      if (l < sz(a)) {
+        tree[x] = a[l];
+      }
+      return;
+    }
+    int mid = l + (r - l) / 2;
+    build(a, 2 * x + 1, l, mid);
+    build(a, 2 * x + 2, mid, r);
+  }
+
+  void build(vector<int> &a) {
+    build(a, 0, 0, sz);
+  }
+
+  long long query(int i, int x, int l, int r) {
+    if (l + 1 == r) {
+      return tree[x];
+    }
+    long long ans = tree[x];
+    int mid = l + (r - l) / 2;
+    if (i < mid) {
+      ans += query(i, 2 * x + 1, l, mid);
+    } else {
+      ans += query(i, 2 * x + 2, mid, r);
+    }
+    return ans;
+  }
+
+  long long query(int i) {
+    return query(i, 0, 0, sz);
+  }
+};
 
 int main() {
   ios::sync_with_stdio(false);
